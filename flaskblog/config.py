f8706b59a407bfec3bbc211b3a907122c1b6ec52
parth <parthbagda4041@gmail.com>
2023-08-05 10:06:37 +0530

    .

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,7 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    postgres://sq_user:Ps1qdZCDbOvEXT4z8HWmgol0jS044XDd@dpg-cj6sll5jeehc73bre14g-a.oregon-postgres.render.com/sq
+    
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True

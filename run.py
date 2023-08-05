149672c91972872b9b9fd35a3265f0f4b9e9ac24
parth <parthbagda4041@gmail.com>
2023-08-05 09:27:47 +0530

    h

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flaskblog import create_app
 app = create_app()
 app.app_context().push()
 if __name__ == '__main__':
-        app.run(debug=True)
+        app.run(debug=False)
          
 
         
